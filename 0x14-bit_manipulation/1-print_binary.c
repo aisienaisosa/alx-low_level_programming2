d7fb78e439ed6bd3b6f12c6898598e076861f2f9
aisienaisosa <106568454+aisienaisosa@users.noreply.github.com>
2022-07-22 23:39:24 +0100

    Create 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void print_binary(unsigned long int n){
+    unsigned int current;
+    int i
+    for(i=7; i>=0; i--){
+            current = 1 << i;
+            while(current&1){
+                _putchar("1")
+            }
+                else{
+                        putchar("0");
+                }
+        
+    }
+
+
+
+
+}
